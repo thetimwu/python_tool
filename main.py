723a74fd2e94c3f09cdd2d928831f8e53fa4fc80
tim wu <timwuuk@gmail.com>
2021-02-27 21:00:25 +0000

    add tuple and find by title

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ driver.get("https://home.oxfordowl.co.uk/reading/free-ebooks/")
 links = driver.find_elements_by_css_selector(".et_pb_text_inner ul li a")
 
 
-levelfolder = 'Oxford Level 16'
+levelfolder = 'Oxford Level 1'
 filepath = "H:\\oxfordowl\\" + levelfolder
 
 if not os.path.isdir(filepath):
@@ -30,8 +30,8 @@ def takes(foldername, filename):
     subfolder = filepath +"\\" + foldername
     if not os.path.isdir(subfolder):
         os.mkdir(subfolder)
-    img = pyautogui.screenshot(imageFilename=subfolder + "\\" + filename + ".png", region=(8, 120, 1250, 840)) #80, 120, 1120, 800
-
+    img = pyautogui.screenshot(imageFilename=subfolder + "\\" + filename + ".png", region=(10, 120, 1250, 800)) #80, 120, 1120, 805
+                                                                                                               #9, 120, 1250, 820
 def loginpage():
     try:
         username = driver.find_element_by_css_selector(
@@ -44,11 +44,11 @@ def loginpage():
         "#parent-eacloginbox #eacLoginWidgetContainer #eacLoginBody #eacBasicLoginForm #eacLoginButton #eacLoginSubmit")
         loginbutton.click()
     except:
-        sleep(2000)
+        sleep(900)
         loginpage()
 
 def turnpage_takescreenshot(foldername):
-    for i in range(19): #36
+    for i in range(9): # pages: 36
         sleep(10)
         takes(foldername, str(i))
         try:
@@ -61,16 +61,19 @@ def turnpage_takescreenshot(foldername):
         nextbutton.click()
 
 def selectbooks():
-    counter = 2
+    counter = 0 #0   6
     sleep(5)
     books = driver.find_elements_by_css_selector(".bookshelf li")
-    #for book in books:
-    for i in range(1):
+    booktitles = ( "Family Poems")
+
+    for book in books:
+    #for i in booktitles:
+    #for i in range(1):
         tagclass = ".pos-" + str(counter) + "-e"
         bk = driver.find_element_by_css_selector(".bookshelf li "+tagclass)
+        #find by title
+        #bk = driver.find_element_by_css_selector(f"[title^='{booktitles[counter-6]}']")
         bk.click()
-        #driver.switch_to.window(driver.window_handles[1])
-        ## e1 = driver.find_element_by_css_selector(".read-ebook-link")
         sleep(2)
         e1 = driver.execute_script("document.getElementsByClassName('read-ebook-link')[0].click()")
 
@@ -120,69 +123,3 @@ for link in links:
 
 #driver.close()
 #driver.quit()
-
-# le = tk.StringVar()
-# top = tk.StringVar()
-# width = tk.StringVar()
-# height = tk.StringVar()
-# p = tk.StringVar()
-# url = tk.StringVar()
-#
-# tk.Label(root, text = "left").grid(row = 0, column=0, sticky='E')
-# tk.Entry(root, textvariable=le).grid(row = 0, column = 1, sticky='W')
-#
-#
-# tk.Label(root, text = "top").grid(row = 1, column=0, sticky='E')
-# tk.Entry(root, textvariable=top).grid(row = 1, column = 1, sticky='W')
-#
-#
-# tk.Label(root, text = "width").grid(row = 2, column=0, sticky='E')
-# tk.Entry(root,textvariable=width).grid(row = 2, column = 1, sticky='W')
-#
-#
-# tk.Label(root, text = "height").grid(row = 3, column=0, sticky='E')
-# tk.Entry(root,textvariable=height).grid(row = 3, column = 1, sticky='W')
-#
-# tk.Label(root, text = "File Path").grid(row = 5, ipady=23)
-# tk.Entry(root,textvariable=p, width="65").grid(row = 5, column = 1)
-#
-#
-# tk.Label(root, text = "MP3 URL").grid(row = 9, ipady=40)
-# tk.Entry(root,textvariable=url, width="65").grid(row = 9, column = 1)
-#
-# def takes():
-#     lv = le.get()
-#     tv = top.get()
-#     wv = width.get()
-#     hv = height.get()
-#     pv = p.get()
-#     img=pyautogui.screenshot(imageFilename=r""+pv+".png", region=(float(lv),float(tv), float(wv), float(hv)))
-#
-#
-# def quitApp():
-#     root.destroy()
-#
-#
-# def download_mp3():
-#     pv = p.get()
-#     pv = pv[:-2]
-#     uv = url.get()
-#
-#     for x in range(1, 12):
-#         new_url = uv.replace('2', str(x))
-#         print(new_url)
-#         doc = requests.get(new_url)
-#         with open('' + pv + str(x) + '.mp3', 'wb') as f:
-#             f.write(doc.content)
-#
-#
-# button1=tk.Button(root, text="Take ScreenShot", command=takes)
-# button1.place(x=60,y=130)
-#
-# button3=tk.Button(root, text="Download mp3", command=download_mp3)
-# button3.place(x=60,y=215)
-#
-# button2=tk.Button(root, text="Quit", command=quitApp)
-# button2.place(x=410,y=270)
-#
-# root.mainloop()
